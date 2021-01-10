a24c5f137fa1f87dd44bff1a94149935143e1b1a
Vanessa Schramm Schenkel da Silva <vanessa.silva@cwi.com.br>
2021-01-10 17:37:36 -0300

    estrutura inicial da api

---
+++ api/src/test/java/com/pt/petinder/PetinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pt.petinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
